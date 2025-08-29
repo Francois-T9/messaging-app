e44e19e05a882c0da25da932560be288909648b8
Francois <francois.thullier98@gmail.com>
2025-08-29 13:41:49 +0200

    created collapsible menu

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -1,24 +1,7 @@
 "use client";
 
-import * as React from "react";
-import {
-  AppWindow,
-  AudioWaveform,
-  Calendar,
-  ChevronDown,
-  Command,
-  GalleryVerticalEnd,
-  Home,
-  Inbox,
-  LogInIcon,
-  LogOutIcon,
-  MessageCircle,
-  Search,
-  Settings,
-} from "lucide-react";
-
-import { NavMain } from "@/components/nav-main";
-import { NavUser } from "@/components/nav-user";
+import { ChevronRight, LogOutIcon, MessageCircle } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Sidebar,
   SidebarContent,
@@ -30,63 +13,84 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
-const items = [
-  {
-    title: "Inbox",
-    url: "/",
-    icon: MessageCircle,
-  },
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@radix-ui/react-collapsible";
 
+const mockData = [
   {
-    title: "Logout",
-    url: "/",
-    icon: LogOutIcon,
+    name: "Francois",
+    online: false,
+  },
+  {
+    name: "Yeyita",
+    online: true,
   },
 ];
-
 export default function AppSidebar() {
   return (
     <Sidebar collapsible="icon">
-      <SidebarHeader>
+      <SidebarHeader className="p-4">
+        <a href="/" className="flex items-center gap-3">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="/logo.png" alt="App Logo" />
+            <AvatarFallback>MA</AvatarFallback>
+          </Avatar>
+        </a>
+      </SidebarHeader>
+      <SidebarContent>
+        <Collapsible
+          key="conversations"
+          title="conversations"
+          defaultOpen
+          className="group/collapsible"
+        >
+          <SidebarGroup>
+            <SidebarGroupLabel
+              asChild
+              className="group/label text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground text-sm"
+            >
+              <CollapsibleTrigger>
+                <MessageCircle />
+                Messages{" "}
+                <ChevronRight className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {mockData.map((user) => (
+                    <SidebarMenuItem key={user.name}>
+                      <SidebarMenuButton asChild>
+                        <a>{user.name}</a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+      </SidebarContent>
+      <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem key="Messaging app">
+          <SidebarMenuItem key="logout">
             <SidebarMenuButton asChild>
               <a href="/">
-                <AppWindow />
-                <h1>Messaging app</h1>
+                <LogOutIcon />
+                <span>Logout</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter></SidebarFooter>
+      </SidebarFooter>
+      <SidebarRail />
     </Sidebar>
   );
 }
