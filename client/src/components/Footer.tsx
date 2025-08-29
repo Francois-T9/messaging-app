eb5522bdced0d14b3216680f58925d1c3def0f34
Francois <francois.thullier98@gmail.com>
2025-08-29 13:14:56 +0200

    created header and footer for home page

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Icon from "@mdi/react";
+import { mdiGithub, mdiLinkedin, mdiEmail } from "@mdi/js";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="fixed bottom-0 flex p-2 justify-between w-screen">
+      <div className="about">
+        {" "}
+        <p>François Thullier 2025</p>
+      </div>
+      <div className="socials flex">
+        <Icon path={mdiGithub} size={1.5} className="cursor-pointer"></Icon>
+        <Icon path={mdiLinkedin} size={1.5} className="cursor-pointer"></Icon>
+        <Icon path={mdiEmail} size={1.5} className="cursor-pointer"></Icon>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { Link } from "react-router";
 function Header() {
   return (
-    <div className="navbar min-h-20 p-3  flex gap-2 items-center justify-between">
+    <div className="navbar min-h-20 p-3  flex gap-2 items-center justify-between ">
       <div className="logo">Messaging app</div>
       <div className="flex  justify-items-center gap-2">
         <ModeToggle />
-        <Button variant={"outline"}>Home</Button>
-        <Button>Get started</Button>
+        <Link to="/">
+          <Button variant={"outline"}>Home</Button>
+        </Link>
+        <Link to="/signup">
+          <Button>Get started</Button>
+        </Link>
       </div>
     </div>
   );
