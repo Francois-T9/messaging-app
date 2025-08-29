85f1e10f4326c007bb7b1ddcdd181727a307be46
Francois <francois.thullier98@gmail.com>
2025-08-29 13:15:47 +0200

    modified default sidebar

---
+++ client/src/components/app-sidebar.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import * as React from "react";
+import {
+  AppWindow,
+  AudioWaveform,
+  Calendar,
+  ChevronDown,
+  Command,
+  GalleryVerticalEnd,
+  Home,
+  Inbox,
+  LogInIcon,
+  LogOutIcon,
+  MessageCircle,
+  Search,
+  Settings,
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+const items = [
+  {
+    title: "Inbox",
+    url: "/",
+    icon: MessageCircle,
+  },
+
+  {
+    title: "Logout",
+    url: "/",
+    icon: LogOutIcon,
+  },
+];
+
+export default function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem key="Messaging app">
+            <SidebarMenuButton asChild>
+              <a href="/">
+                <AppWindow />
+                <h1>Messaging app</h1>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter></SidebarFooter>
+    </Sidebar>
+  );
+}
