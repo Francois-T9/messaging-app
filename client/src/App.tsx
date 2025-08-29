f1408722d168b90c696bf517553b5818aaa51d57
Francois <francois.thullier98@gmail.com>
2025-08-29 13:14:35 +0200

    changed app router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import { ThemeProvider } from "./context/theme-provider";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/",
       element: <Layout />,
       children: [
         {
-          path: "/",
-          element: <Home />,
+          path: "/dashboard",
+          element: <Dashboard />,
         },
       ],
     },
