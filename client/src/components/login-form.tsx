47124336fb18fa82714d969ea79b6d465bac5157
Francois <francois.thullier98@gmail.com>
2025-08-29 13:15:12 +0200

    created form woth zod and react hook form

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -7,13 +7,42 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useForm, type Control, type FieldPath } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(2).max(50),
+});
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <div className={cn("flex flex-col gap-6 w-1/3", className)} {...props}>
       <Card>
@@ -24,44 +53,66 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
-          </form>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <LoginFormField
+                name="username"
+                label="Username"
+                placeholder="John Doe"
+                inputType="text"
+                formControl={form.control}
+              />
+              <LoginFormField
+                name="password"
+                label="Password"
+                inputType="password"
+                formControl={form.control}
+              />
+              <Button className="w-full" type="submit">
+                Log in
+              </Button>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
   );
 }
+
+type LoginFormFieldProps = {
+  name: FieldPath<z.infer<typeof formSchema>>;
+  label: string;
+  placeholder?: string;
+  description?: string;
+  inputType?: string;
+  formControl: Control<z.infer<typeof formSchema>>;
+};
+const LoginFormField: React.FC<LoginFormFieldProps> = ({
+  name,
+  label,
+  placeholder,
+  description,
+  inputType,
+  formControl,
+}) => {
+  return (
+    <FormField
+      control={formControl}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={inputType || "text"}
+              {...field}
+            ></Input>
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage></FormMessage>
+        </FormItem>
+      )}
+    />
+  );
+};
